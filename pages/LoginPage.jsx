9e30c88a38db51743a25101fd05d79b74d3a5c6e
Elif Aydınlık <elifaydnlk@hotmail.com>
2025-06-05 18:52:27 +0300

    Working on pages

--- pages/LoginPage.jsx
+++ pages/LoginPage.jsx
@@ -1,13 +1,71 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {auth} from "../firebase/auth";
-import { getFirestore,doc,getDoc } from "firebase/firestore";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
+import busimg from "../src/assets/busimg.gif";
+import calendarimg from "../src/assets/calendarimg.jpg";
+
+
 function LoginPage() {
+
+    //hold user info
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    //right-side current card state
+    const [currentCard, setCurrentCard] = useState(0);
+
+    //navigate user based on roles in firestore
+    const navigate = useNavigate();
+    const db = getFirestore();
+
+    //shuttle and calendar cards on the right side
+    const cards = [
+
+        {
+            title: "Academic Calendar",
+            description: "View academic dates and deadlines.",
+            image: calendarimg,
+            link: "/calendar",
+        },
+        {
+            title: "Shuttle Hours",
+            description: "Check and filter shuttle and ring services",
+            image: busimg,
+            link: "/shuttle",
+        },
+
+    ];
+
+    //async function for user roles
+    const handleLogin = async (e) => {
+        //prevent reload
+        e.preventDefault();
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const uid = userCredential.user.uid;
+            const roleDocument = await getDoc(doc(db, "roles", uid));
+            if (roleDocument.exists()) {
+                const role = roleDocument.data().role;
+                if (role === "admin") navigate("/admin");
+                else if (role === "staff") navigate("/staff");
+                else navigate("/student");
+            } else {
+                alert("Unable to find user role.");
+            }
+        }
+        catch (error) {
+            console.error(error);
+            alert("Unable to login.");
+        }
+    };
+
     return (
-        <div></div>
+        <div/>
     );
+
 }
+
 
 export default LoginPage;
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route,Routes } from 'react-router-dom';
+
+import LoginPage from '../pages/LoginPage';
+import StaffPage from '../pages/StaffPage';
+import AdminPage from '../pages/AdminPage';
+import StudentPage from '../pages/StudentPage';
+import ProtectedRoute from '../components/ProtectedRoute';
+import AcademicCalendar from '../pages/AcademicCalendar';
+import ShuttleHours from '../pages/ShuttleHours';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-              <p>
-        EduStack Homepage
-      </p>
-      </div>
-    </>
-  )
+    <Routes>
+
+      <Route path='/' element={<LoginPage/>} />
+
+      <Route path='/admin' element={
+        <ProtectedRoute>
+        <AdminPage/>
+      </ProtectedRoute>} />
+
+      <Route path='/student' element={
+        <ProtectedRoute>
+          <StudentPage/>
+        </ProtectedRoute>}/>
+
+      <Route path='staff' element={
+        <ProtectedRoute>
+          <StaffPage/>
+        </ProtectedRoute>} />
+
+      <Route path='/calendar' element={<AcademicCalendar/>} />
+      <Route path='/shuttle' element={<ShuttleHours/>}/>
+      
+      </Routes>
+      
+   );
 }
 
-export default App
+export default App;
